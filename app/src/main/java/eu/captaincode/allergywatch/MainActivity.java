e213f4620d3d2a5a0c1ba48caf3b5e638c0e8de2
mrpeny <bence.konya@captaincode.eu>
2018-09-18 22:29:57 +0200

    feat: Barcode reading with Camera intent

--- app/src/main/java/eu/captaincode/allergywatch/MainActivity.java
+++ app/src/main/java/eu/captaincode/allergywatch/MainActivity.java
@@ -1,15 +1,17 @@
 package eu.captaincode.allergywatch;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
-import com.google.android.gms.common.images.internal.ImageUtils;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,30 +22,61 @@ import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    TextView barcodeResultTextView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String mCurrentPhotoPath;
+    private Uri mPhotoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        barcodeResultTextView = findViewById(R.id.tv_barcode_result);
-        Bitmap barcode1 = BitmapFactory.decodeResource(getResources(), R.drawable.barcode1);
-        detectBarcode(barcode1);
     }
 
-    private void detectBarcode(Bitmap barcode) {
+    public void takePhoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                mPhotoURI = FileProvider.getUriForFile(this,
+                        "eu.captaincode.allergywatch.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private void detectBarcode() {
         FirebaseVisionBarcodeDetectorOptions options =
                 new FirebaseVisionBarcodeDetectorOptions.Builder().setBarcodeFormats(
                         FirebaseVisionBarcode.FORMAT_EAN_13
                 ).build();
 
-        FirebaseVisionImage firebaseImageBarcode = FirebaseVisionImage.fromBitmap(barcode);
+        FirebaseVisionImage firebaseImageBarcode = null;
+        try {
+            firebaseImageBarcode = FirebaseVisionImage.fromFilePath(this, mPhotoURI);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
                 .getVisionBarcodeDetector(options);
@@ -54,7 +87,9 @@ public class MainActivity extends AppCompatActivity {
                     public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
                         Log.d(LOG_TAG, "Imagedetection.onSuccess()");
                         for (FirebaseVisionBarcode barcode : firebaseVisionBarcodes) {
-                            Log.d(LOG_TAG, "Detected barcode: " + barcode.getRawValue());
+                            String rawValue = barcode.getRawValue();
+                            Log.d(LOG_TAG, "Detected barcode: " + rawValue);
+                            Toast.makeText(MainActivity.this, rawValue, Toast.LENGTH_LONG).show();
                         }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
@@ -72,5 +107,30 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(LOG_TAG, "onActivityResult() with code: " + requestCode);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            detectBarcode();
+        }
     }
 }
